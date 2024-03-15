b9dcdcbb51a711ad3cbfc27be23734bfd4cedbcb
Hooda AbdulShakoor <hoodaabdulshakoor@gmail.com>
2024-03-15 23:10:34 +0400

    exercises

--- test2/nextjs-test/app/page.tsx
+++ test2/nextjs-test/app/page.tsx
@@ -3,12 +3,17 @@ import React from 'react';
 
 const FragrancesPage = () => {
   return (
-    <div className="bg-blue-100 min-h-screen flex flex-col justify-center items-center">
+    <div className="bg-gradient-to-t to-yellow-100 from-purple-400 flex flex-col text-black min-h-screen justify-center items-center relative">
       <Head>
         <title>Fragrances</title>
       </Head>
+
+      {/* Heading */}
+      <h1 className="[rgba(58, 36, 113, 1)] font-poppins text-[64px] font-semibold text-center mb-40 px-8 absolute top-0 p-5">Exercises</h1>
+
+
       {/* Top Left Logo */}
-      <div className="absolute top-0 left-0 p-2">
+      <div className="absolute top-0 right-0 p-5">
         <img
           src="/logo.png"
           alt="Logo"
@@ -17,11 +22,11 @@ const FragrancesPage = () => {
       </div>
 
       {/* Top Right Back Button */}
-      <div className="absolute top-5 right-5">
+      <div className="absolute top-10 left-5">
         <button className="px-4 py-2 text-gray-800 rounded-lg">
           <span className="flex items-center">
             <svg
-              className="w-4 h-4 inline-block ml-1 -mt-1"
+              className="w-10 h-10 inline-block ml-1 -mt-1"
               fill="none"
               stroke="currentColor"
               viewBox="0  0  24  24"
@@ -31,30 +36,21 @@ const FragrancesPage = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M10  19l-7-7m0  0l7-7m-7  7h18"
+                d="M10  19l-7-7m0  0l7-7m-7  7h"
               ></path>
             </svg>
-            <span className="ml-2">Back</span>
           </span>
         </button>
       </div>
 
-      {/* Bottom Right Small Logo */}
-      <div className="absolute bottom-0 right-0 p-5">
-        <div className="w-[102px] h-[102px] border border-gray-700"></div>
-      </div>
 
       {/* Main Body */}
-      <div className="grid grid-cols-4 grid-rows-2 gap-10 mt-30">
+      <div className="grid grid-cols-4 gap-10 mt-30">
         {/* Fragrance Items */}
-        <FragranceItem title="Lavender" imageSrc="../images/MainMenuImages/Fragrances.svg" />
-        <FragranceItem title="Rosemary" imageSrc="../images/MainMenuImages/Fragrances.svg" />
-        <FragranceItem title="Rose" imageSrc="../images/MainMenuImages/Fragrances.svg" />
-        <FragranceItem title="Chamomile" imageSrc="../images/MainMenuImages/Fragrances.svg" />
-        <FragranceItem title="Geranium" imageSrc="../images/MainMenuImages/Fragrances.svg" />
-        <FragranceItem title="Chamomile" imageSrc="../images/MainMenuImages/Fragrances.svg" />
-        <FragranceItem title="Lavender" imageSrc="../images/MainMenuImages/Fragrances.svg" />
-        <FragranceItem title="Rose" imageSrc="../images/MainMenuImages/Fragrances.svg" />
+        <FragranceItem title="Zen Calm" imageSrc="../images/MainMenuImages/Exercises.svg" />
+        <FragranceItem title="Mindfulness Journey" imageSrc="../images/MainMenuImages/Exercises.svg" />
+        <FragranceItem title="Serenity Now" imageSrc="../images/MainMenuImages/Exercises.svg" />
+        <FragranceItem title="Tranquil Waters" imageSrc="../images/MainMenuImages/Exercises.svg" />
       </div>
     </div>
   );
@@ -67,12 +63,17 @@ interface FragranceItemProps {
 
 const FragranceItem: React.FC<FragranceItemProps> = ({ title, imageSrc }) => {
   return (
-    <div className="w-52 h-52 flex flex-col items-center bg-white rounded-lg shadow-md">
-      <img src={imageSrc} alt={title} className="w-32 h-32 mt-4" />
-      <p className="font-poppins text-base font-normal mt-4">{title}</p>
+    <div className="flex flex-col items-center">
+      <div className="w-220 h-79 bg-white rounded-lg shadow-md flex justify-center items-center p-5">
+        <img src={imageSrc} alt={title} className="w-32 h-32" />
+      </div>
+      <p className="font-poppins text-30 font-semibold leading-42 text-center mt-4 text-black">{title}</p>
     </div>
   );
 };
 
-
 export default FragrancesPage;
+
+
+
+
